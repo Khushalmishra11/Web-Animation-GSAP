f0c7ecc9699f0fd53daa536797ff9ffa5e5352f4
Khushal Mishra <khushalmishra261203@gmail.com>
2025-02-09 12:45:31 +0530

    ADDED CURSOR

--- script.js
+++ script.js
@@ -36,6 +36,14 @@ function init() {
 
 init();
 
+
+var crsr = document.querySelector(".cursor")
+var main = document.querySelector(".main")
+main.addEventListener("mousemove",function(dets)){
+  crsr.style.left = dets.x+"px"
+  crsr.style.top = dets.y+"px"
+}
+
 var tl = gsap.timeline({
     scrollTrigger:{
         trigger:".page1 h1",
